3fb96d5d3f5445af4063c908f90dd77c4f17dd5b
abhishekwl <nateriver210@gmail.com>
2018-05-31 09:29:19 +0530

    Added IP address support

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const config = require("./config");
+const ip = require("ip");
 
 const PORT = process.env.PORT || 8000;
 const app = express();
@@ -13,6 +14,8 @@ app.use(bodyParser.json());
 mongoose.Promise = global.Promise;
 mongoose.connect(config.dbUri).then(()=>{
     console.log("[MONGODB] Server sucessfully hooked to DB");
+    console.log("[/DEBUG]")
+    console.log("-----------------------------------------------------------------");
 }).catch(err=>{
     console.log("[MONGODB] Could not connect to database. ERR: "+err);
     process.exit();
@@ -24,5 +27,8 @@ app.get('/', (req,res)=>{
 
 require("./app/routes/test.routes.js")(app);
 app.listen(PORT, "0.0.0.0", ()=>{
+    console.log("-----------------------------------------------------------------");
+    console.log("[DEBUG]");
     console.log("[SERVER] Listening on port "+PORT);
+    console.log("[ENDPOINT TEST] http://"+ip.address()+":"+PORT+""+config.apiRoute+"/tests");
 });
