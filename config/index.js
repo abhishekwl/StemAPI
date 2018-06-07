3a9c81f63d563b736750a79aba7aa5a7642e1633
abhishekwl <nateriver210@gmail.com>
2018-06-07 20:47:51 +0530

    Moved DB to MLab

--- config/index.js
+++ config/index.js
@@ -1,4 +1,7 @@
 module.exports = {
-    dbUri: "mongodb://localhost:27017/stem",
-    apiRoute: "/api/v1"
+    apiRoute: "/api/v1",
+    users: {
+        "abhishekwl": "abcd1234"
+    },
+    dbUri: "mongodb://abhishekwl:abcd1234@ds016108.mlab.com:16108/stem_api",
 };
